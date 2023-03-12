06e0961397adb1b6096d8f82ad3c2ed1a2c62d22
ana-ge <ge.anatoliy@gmail.com>
2023-03-12 20:57:00 +0900

    Adding PizzaScanner App project

---
+++ PizzaScanner/ViewController.swift
@@ -0,0 +1,74 @@
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedimage
+            
+            guard let ciimage = CIImage(image: userPickedimage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+            
+        }
+        
+        imagePicker.dismiss(animated: true)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResut = results.first {
+                if firstResut.identifier.contains("pizza") {
+                    self.navigationItem.title = "Pizza! 🍕"
+                } else {
+                    self.navigationItem.title = "Not Pizza! ❌"
+                }
+            }
+
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true)
+        
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# PizzaScanner
+
+It's a simple app which helps to determine if there is a pizza on photo or not. In this app i used CoreML technology which contains his own pre-trained model. This model helps us to identify pizza. So our model contains a lot of results with probabilities and we are using the highest one to identify our pizza. 
